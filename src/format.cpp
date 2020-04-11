594b4aa9174a68de6148c3d8582822a0ccdad2d3
acherep <acherep@mail.ru>
2020-04-11 21:37:00 +0200

    Memory utilization, kernel, total and running processes, uptime

--- src/format.cpp
+++ src/format.cpp
@@ -2,10 +2,24 @@
 
 #include <string>
 
-using std::string;
+using std::string, std::to_string;
 
-// TODO: Complete this helper function
+string addLeadingZero(int value)
+{
+    if (value >= 0 && value < 10)
+    {
+        return "0" + to_string(value);
+    }
+    return to_string(value);
+}
+
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds [[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long times)
+{
+    int seconds = times % 60;
+    long hours = times / (60 * 60);
+    int minutes = (times - hours * 60 * 60) / 60;
+
+    return addLeadingZero(hours) + ":" + addLeadingZero(minutes) + ":" + addLeadingZero(seconds);
+}
